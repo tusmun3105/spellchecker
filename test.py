813e5c02d314267f1be8f9e1585680841595065c
tusmun <128845167+tusmun@users.noreply.github.com>
2023-07-17 11:29:19 +0400

    Update test.py

--- test.py
+++ test.py
@@ -16,8 +16,8 @@ import json
 from soundex import find_matching_soundex
 config = configparser.ConfigParser()
 config.read('static\config.ini')
-connectionstring = config.get('DEFAULT', 'DBConnectionString')
 apiURL = config.get('DEFAULT', 'api_URL')
+connectionstring = config.get('DEFAULT', 'DBConnectionString')
 # with open('static\sorted_words.txt', 'r') as f:
 #    words = [line.strip() for line in f]
 words = []
