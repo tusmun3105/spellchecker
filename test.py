057dac3c7566d205fd0c292d85eef63aafc0b206
tusmun <128845167+tusmun@users.noreply.github.com>
2023-07-17 21:18:08 +0400

    Update test.py

--- test.py
+++ test.py
@@ -14,12 +14,14 @@ import re
 import configparser
 import json
 from soundex import find_matching_soundex
+print("helllllllllllllllllllllllllllllllllllllllllllllllllllllo")
 config = configparser.ConfigParser()
 config.read('static\config.ini')
 # config.get('DEFAULT', 'api_URL')
 apiURL = 'https://api.api-ninjas.com/v1/imagetotext'
 # config.get('DEFAULT', 'DBConnectionString')
 connectionstring =os.getenv('MONGODB_URI')
+print(connectionstring)
 #'mongodb+srv://tushaar0011:Tushaar0011@cluster0.2axzrgw.mongodb.net/?retryWrites=true&w=majority'
 # with open('static\sorted_words.txt', 'r') as f:
 #    words = [line.strip() for line in f]
