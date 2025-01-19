f20e3787bf704319c58ef60c94604e37915663ae
tusmun3105 <150321049+tusmun3105@users.noreply.github.com>
2025-01-19 17:29:02 +0400

    Update test.py

--- test.py
+++ test.py
@@ -825,6 +825,6 @@ def processVal(value):
         return jsonify(response)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+#if __name__ == "__main__":
+    #app.run(host="0.0.0.0", port=5000)
     # app.run(debug=True)
